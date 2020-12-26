3a1601e4926153631fb04ef56af832766529c1d1
Kumar Nikhil <knikhil352000@gmail.com>
2020-12-26 22:48:26 +0530

    State and Text Inputs

--- App.js
+++ App.js
@@ -1,31 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from "expo-status-bar";
+import React, {useState} from "react";
+import {Button, StyleSheet, Text, TextInput, View} from "react-native";
 
 export default function App() {
-  const [name, setName] = useState('nikhil');
+  const [name, setName] = useState("nikhil");
+  const clickHandler = () => {
+    setName('Nikhil Singh');
+  }
+
+
   return (
     <View style={styles.container}>
+      <Text>Enter name:</Text>
+      <TextInput 
+        style={styles.input}
+        placeholder='eg Nikhil'
+        onChangeText={(val) => setName(val)}
+        keyboardType='numeric'/>
       <Text>My name is {name}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="update state" onPress={clickHandler}/>
+      </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  header: {
-    backgroundColor: 'pink',
-    padding: 20,
+  buttonContainer: {
+    marginTop: 20
   },
-  boldText: {
-    fontWeight: 'bold'
-  },
-  body: {
-    backgroundColor: 'yellow'
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200
+
   }
 });
