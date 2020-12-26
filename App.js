c31b1a978ee31fc105afb44b202408af1f9bda3c
Kumar Nikhil <knikhil352000@gmail.com>
2020-12-26 23:20:06 +0530

    List and ScrollView

--- App.js
+++ App.js
@@ -1,27 +1,36 @@
 import {StatusBar} from "expo-status-bar";
 import React, {useState} from "react";
-import {Button, StyleSheet, Text, TextInput, View} from "react-native";
+import {Button, ScrollView, StyleSheet, Text, TextInput, View} from "react-native";
 
 export default function App() {
-  const [name, setName] = useState("nikhil");
-  const clickHandler = () => {
-    setName('Nikhil Singh');
-  }
+  const [people, setPeople] = useState([
+    {name: 'Shankar', key: '1'},
+    {name: 'Amit', key: '2'},
+    {name: 'Sandharv', key: '3'},
+    {name: 'Shinigami', key: '4'},
+    {name: 'Naruto', key: '5'},
+    {name: 'Rakesh', key: '6'},
+    {name: 'Rakesh', key: '6'},
+    {name: 'Rakesh', key: '6'},
+    {name: 'Rakesh', key: '6'},
+  ])
 
 
   return (
     <View style={styles.container}>
-      <Text>Enter name:</Text>
-      <TextInput 
-        style={styles.input}
-        placeholder='eg Nikhil'
-        onChangeText={(val) => setName(val)}
-        keyboardType='numeric'/>
-      <Text>My name is {name}</Text>
-      <View style={styles.buttonContainer}>
-        <Button title="update state" onPress={clickHandler}/>
-      </View>
+      <ScrollView>
+      {
+        people.map((item) => {
+          return (
+            <View key={item.key}>
+              <Text style={styles.item}>{item.name}</Text>
+            </View> 
+          )
+        })
+      }
+      </ScrollView>
     </View>
+    
   );
 }
 
@@ -29,18 +38,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: "center",
+    // justifyContent: "center",
   },
-  buttonContainer: {
-    marginTop: 20
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200
-
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor:'pink',
+    fontSize:24
   }
 });
