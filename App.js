2ceae800df43aee99329f0f11160a6e5a721e0be
Kumar Nikhil <knikhil352000@gmail.com>
2020-12-27 23:22:18 +0530

    Added Header in Todo

--- App.js
+++ App.js
@@ -1,40 +1,28 @@
 import {StatusBar} from "expo-status-bar";
 import React, {useState} from "react";
 import {Button, FlatList, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
+import Header  from "./components/Header";
 
 export default function App() {
-  const [people, setPeople] = useState([
-    {name: 'Shankar', id: '1'},
-    {name: 'Amit', id: '2'},
-    {name: 'Sandharv', id: '3'},
-    {name: 'Shinigami', id: '4'},
-    {name: 'Naruto', id: '5'},
-    {name: 'Rakesh', id: '6'},
-    {name: 'Nikhil', id: '7'},
-    {name: 'Intrinsic', id: '8'},
-    {name: 'Honey', id: '9'},
+  const [todos, setTodos] = useState([
+    {text: 'buy coffee', key: '1'},
+    {text: 'create an app', key: '2'},
+    {text: 'play on switch', key: '3'}
   ])
-  const pressHandler = (id) => {
-    console.log(id);
-    setPeople((prevPeople) => { 
-      return prevPeople.filter(person => person.id != id);
-    })
-  }
-
-
   return (
     <View style={styles.container}>
-      <FlatList 
-        keyExtractor={(item) => item.id}
-        numColumns={2}
-        data={people}
-        renderItem={({item}) => (
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
-
+      {/* header  */}
+      <Header />
+      <View style={styles.content}>
+        {/* to form  */}  
+        <View style={styles.list}>
+          <FlatList 
+            data={todos}
+            renderItem={({ item }) => (
+              <Text>{item.text}</Text>
+            )}/>
+        </View>
+      </View>
     </View>
     
   );
@@ -44,17 +32,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingTop: 40,
-    paddingHorizontal: 20,
-    // alignItems: "center",
-    // justifyContent: "center",
   },
-  item: {
-    
-    padding: 30,
-    backgroundColor:'pink',
-    fontSize:24,
-    margin: 8,
-    width:150
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   }
 });
---
+++ components/Header.js
@@ -0,0 +1,25 @@
+import {StatusBar} from "expo-status-bar";
+import React, {useState} from "react";
+import {Button, FlatList, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
+
+export default function Header() {
+    return (
+        <View style={styles.header}>
+            <Text style={styles.title}>My Todos</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        height:80,
+        paddingTop: 38,
+        backgroundColor:'coral'
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#fff'
+    }
+})
