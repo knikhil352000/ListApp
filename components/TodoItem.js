d4efe4841dd4a012952adc8a320e2e80ff90367e
Kumar Nikhil <knikhil352000@gmail.com>
2020-12-28 22:14:05 +0530

    Material Icon and wrappingthe list App

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,11 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { Button, FlatList, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
-
+import { MaterialIcons } from '@expo/vector-icons';
 export default function TodoItem({ item, pressHandler }) {
     return (
-        <TouchableOpacity style={styles.item} onPress={() => pressHandler(item.key)}>
-            <Text>{item.text}</Text>
+        <TouchableOpacity onPress={() => pressHandler(item.key)}>
+            <View style={styles.item}>
+                <MaterialIcons name='delete' size={18} color='coral'/>
+                <Text style={{marginLeft: 10}}>{item.text}</Text>
+            </View>
+            
         </TouchableOpacity>
     )
 }
@@ -17,6 +21,8 @@ const styles = StyleSheet.create({
         borderColor: '#bbb',
         borderWidth:1,
         borderStyle: 'dashed',
-        borderRadius: 10
+        borderRadius: 10,
+        flexDirection:'row',
+        
     }
 })
